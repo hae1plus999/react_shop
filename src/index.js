90c1ac863663842d1e4a46b462ca1479602f6782
haewon999 <1000hae1@naver.com>
2023-05-01 21:28:34 +0900

    중간 커밋

--- src/index.js
+++ src/index.js
@@ -1,14 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Home from './pages/Home';
+import AllProducts from './pages/AllProducts';
+import ProductDetail from './pages/ProductDetail';
+import NewProduct from './pages/NewProduct';
+import MyCart from './pages/MyCart';
+import NotFound from './pages/NotFound';
 import reportWebVitals from './reportWebVitals';
+import ProtectedRoute from './pages/ProtectedRoute';
+
+const router = createBrowserRouter([{
+    path: '/',
+    element: < App / > ,
+    errorElement: < NotFound / > ,
+    children: [
+        { index: true, path: '/', element: < Home / > },
+        { path: '/products', element: < AllProducts / > },
+        {
+            path: '/products/new',
+            element: ( <
+                ProtectedRoute requireAdmin >
+                <
+                NewProduct / >
+                <
+                /ProtectedRoute>
+            )
+        },
+        { path: '/product/:id', element: < ProductDetail / > },
+        {
+            path: '/carts',
+            element: ( <
+                ProtectedRoute >
+                <
+                MyCart / >
+                <
+                /ProtectedRoute>
+            )
+        },
+    ],
+}, ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+root.render( <
+    React.StrictMode >
+    <
+    RouterProvider router = { router }
+    /> <
+    /React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
